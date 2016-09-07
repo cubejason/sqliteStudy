671819cd0cc1c290de371e2fb1eaaf78ebef7b1e
cubejason <cube.jason.lin@gmail.com>
2016-09-07 17:37:31 +0800

    初次上传，新建空文件

---
+++ sqlite3Demo/sqlite3Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sqlite3Demo
+//
+//  Created by mac-mini2 on 16/9/7.
+//  Copyright © 2016年 jason. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ sqlite3Demo/sqlite3DemoTests/sqlite3DemoTests.m
@@ -0,0 +1,39 @@
+//
+//  sqlite3DemoTests.m
+//  sqlite3DemoTests
+//
+//  Created by mac-mini2 on 16/9/7.
+//  Copyright © 2016年 jason. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface sqlite3DemoTests : XCTestCase
+
+@end
+
+@implementation sqlite3DemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
